023e36b817097c9cd0144b0a3aaf956473012ef1
prosy <pdarceno@gmail.com>
2021-04-16 20:25:26 +0800

    add new hashrouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import logo from './logo.svg';
 // Import the BrowserRouter, Route and Link components
-import { BrowserRouter, Route } from 'react-router-dom'; 
+import { HashRouter, Route } from 'react-router-dom'; 
 import Header from './components/header/Header'; 
 import Background from './pages/background/Background.js'; 
 import Experience from './pages/experience/Experience.js'; 
@@ -15,18 +15,18 @@ import './App.css';
 
 function App() {
 	return (
-		<BrowserRouter>
+		<HashRouter basename='/'>
 			<div className="App">
 				<Header />
 
-				<Route path="/pdarceno-react" component={Home} />
-				<Route path="/pdarceno-react/objectives" component={Objectives} />
-				<Route path="/pdarceno-react/experience" component={Experience} />
-				<Route exact path="/pdarceno-react/background" component={Background} />
-				<Route path="/pdarceno-react/skills" component={Skills} />
-				<Route path="/pdarceno-react/contacts" component={Contacts} />
+				<Route path="/" component={Home} />
+				<Route path="/objectives" component={Objectives} />
+				<Route path="/experience" component={Experience} />
+				<Route exact path="/background" component={Background} />
+				<Route path="/skills" component={Skills} />
+				<Route path="/contacts" component={Contacts} />
 			</div>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,22 @@
 import React from "react";
 
 // Import the BrowserRouter, Route and Link components
-import { Link } from 'react-router-dom'; 
+import { HashRouter, Link } from 'react-router-dom'; 
 
 function Header(props) {
 	return (
-		<div className="navigation">
-			<div className="navigation-sub">
-				<Link to="/pdarceno-react" className="item">Start</Link>
-				<Link to="/pdarceno-react/objectives" className="item">Objectives</Link>
-				<Link to="/pdarceno-react/experience" className="item">Experience</Link>
-				<Link to="/pdarceno-react/background" className="item">Education & Background</Link>
-				<Link to="/pdarceno-react/skills" className="item">Skills & Abilities</Link>
-				<Link to="/pdarceno-react/contacts" className="item">Contacts</Link>
+		<HashRouter basename="/">
+			<div className="navigation">
+				<div className="navigation-sub">
+					<Link to="/" className="item">Start</Link>
+					<Link to="/objectives" className="item">Objectives</Link>
+					<Link to="/experience" className="item">Experience</Link>
+					<Link to="/background" className="item">Education & Background</Link>
+					<Link to="/skills" className="item">Skills & Abilities</Link>
+					<Link to="/contacts" className="item">Contacts</Link>
+				</div>
 			</div>
-		</div>
+		</HashRouter>
 	);
 }
 
