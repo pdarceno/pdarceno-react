4259a27bc328d52596e547cd6c61d3b684c95b13
prosy <pdarceno@gmail.com>
2021-05-01 02:03:22 +0800

    add new css skills

--- src/pages/skills/Skills.js
+++ src/pages/skills/Skills.js
@@ -6,42 +6,42 @@ function Skills(props) {
 		<div>
 			<h1>SKILLS AND ABILITIES</h1>
 			<SkillsComponent
-				image=""
+				image="/images/skills/frontend.jpg"
 				skill="Frontend Development"
 				description="I first learned frontend and web design by doing static web pages in my college years. I learned a lot since then and I've been applying most of them until now."
 			/>
 			<SkillsComponent
-				image=""
+				image="/images/skills/backend.jpeg"
 				skill="Backend Development"
 				description="I learned backend along with database management in college as well. I studied it for quite a while and now have a pretty good grasp on a few best practices and optimizations when retrieving data from a backend server."
 			/>
 			<SkillsComponent
-				image=""
+				image="/images/skills/fullstack.jpg"
 				skill="Fullstack Development"
 				description="After learning front and back, I went on and tried full stack. It was admittedly hard, but the past learnings proved useful and I was able to be part of a team that creates not only responsive but fast apps."
 			/>
 			<SkillsComponent
-				image=""
+				image="/images/skills/javascript.png"
 				skill="Javascript"
 				description="I was able to use vanilla Javascript in college for making responsive web applications. I still use it until now along with a few libraries built around it."
 			/>
 			<SkillsComponent
-				image=""
+				image="/images/skills/php.png"
 				skill="PHP"
 				description="I had my try on PHP in college. This was when we were making apps with actual data and a database. I didn't know any PHP frameworks yet but back then I thought plain PHP was still more than enough."
 			/>
 			<SkillsComponent
-				image=""
+				image="/images/skills/react.png"
 				skill="React"
 				description="I had my first look at React during my internship at PHIVOLCS. I happen to like it but I felt I wasn't able to study it for long because of the short internship time. Fortunately, the first work I had encouraged React for frontend development."
 			/>
 			<SkillsComponent
-				image=""
+				image="/images/skills/laravel.jfif"
 				skill="Laravel"
 				description="Alongside React, I had the opportunity to learn Laravel for the backend processes. I thought it was handy because of its many features but most importantly because of its security and ease of error handling with its error logging ability."
 			/>
 			<SkillsComponent
-				image=""
+				image="/images/skills/webhook.png"
 				skill="Webhook"
 				description="I was also able to use Webhooks for the applications we, in the team, are making. I found it so useful and efficient because we can make the app communicate with itself. It proved useful with so many of the features our clients have been asking for to implement."
 			/>
--- src/pages/skills/SkillsComponent.js
+++ src/pages/skills/SkillsComponent.js
@@ -3,12 +3,16 @@ import React from 'react'; // Import the Component component from React
 
 function SkillsComponent(props) {
 	return (
-		<div className="skills">
-			<div className="skills-image">
-				<img src={props.image} alt={props.skill}/>
+		<div className="skill">
+			<div className="skill-inner">
+				<div className="skill-front">
+					<img className="skill-image" src={process.env.PUBLIC_URL + props.image} alt={props.skill}/>
+				</div>
+				<div className="skill-back">
+					<div className="skill-title">{props.skill}</div>
+					<div className="skill-company">{props.description}</div>
+				</div>
 			</div>
-			<div className="skills-title">{props.skill}</div>
-			<div className="skills-company">{props.description}</div>
 		</div>
 	);
 }
